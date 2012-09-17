90708d2ee95cb5ae3f12d5222d3c8e16185a7717
TurtleKitty <joshua@anwu.org>
2012-09-17 05:40:06 -0400

    Rough draft of datastore wrapper

--- main.rkt
+++ main.rkt
@@ -5,37 +5,53 @@
 ;(provide create-datastore)
 
 ; create datastore
-; delete datastore
 ; create relation name (fields)
 ; delete relation
-; create tuple (x y z)
+; create tuples ((x y z)(p d q))
 ; read (x y) from relation where (u v)
 ; update (x y) in relation where (u v)
 ; delete from relation where (u v)
 
-#|
 
-(define (create-datastore (init #f))
-    (define (build-store (rs #f) (is #f))
-	(define relations (or rs (hash)))
-	(define indexes (or is (hash)))
-	(define (datastore cmd . args)
+(define (create-datastore (rs #f) (is #f))
+    (define relations (or rs (hash)))
+    (define indexes (or is (hash)))
+    (define (serialize)
+	(hash 'relations relations 'indexes indexes))
+    (define (relvar name)
+	(hash-ref relations name))
+    (define (rebuild name fn)
+	(create-datastore
+	    (hash-set relations name (fn) indexes)))
+    (define (add-relation name fields (tuples #f))
+	(rebuild name (λ () (relation name fields tuples))))
+    (define (add-tuples relname tupz)
+	(rebuild relname (λ () ((relvar relname) 'create tupz))))
+    (define (rm-tuples relname where)
+	(rebuild relname (λ () ((relvar relname) 'delete where))))
+    (define (update-tuples relname where put)
+	(rebuild relname (λ () ((relvar relname) 'update where put))))
+    (define (query result-type relname where want)
+	(rebuild relname (λ () ((relvar relname) result-type where want))))
+    (case-lambda
+	(() (serialize))
+	((cmd)
 	    (case cmd
 		('relations relations)
-		('indexes indexes)
-		('add_relation
-		    (build-store
-			(hash-set relations
-			    (car args)
-			    (create-relation (cadr args)))
-			indexes))
-		('add_tuples ())
-	)))
-    (if init
-	(build-store (init 'relations) (init 'indexes))
-	(build-store)))
-
-|#
+		('indexes indexes)))
+	((cmd x)
+	    (case cmd
+		('get_relation (hash-ref relations x))))
+	((cmd x y)
+	    (case cmd
+		('add_relation (add-relation x y))
+		('create (add-tuples x y))
+		('delete (rm-tuples x y))))
+	((cmd x y z)
+	    (case cmd
+		('add_relation (add-relation x y z))
+		('(read read-row read-col read-val) (query cmd x y z))
+		('update (update-tuples x y z))))))
 
 (define (list->fector lst)
     (apply fector lst))
@@ -48,11 +64,10 @@
     (for/vector ((i (in-range 0 (fector-length fv)))) 
 	(fector-ref fv i)))
 
-(define (make-index fields)
-    (for/list ((i (in-range (length fields))) (f (in-list fields)))
-	(cons f i)))
-
-(define (make-lookup index)
+(define (make-lookup fields)
+    (define index
+	(for/list ((i (in-range (length fields))) (f (in-list fields)))
+	    (cons f i)))
     (λ (f) (cdr (assoc f index))))
 
 (define (tuple lookup seq)
@@ -71,13 +86,11 @@
 	    (else (fector->list fvec)))))
 
 (define (tuple-factory fields)
-    (define index (make-index fields))
-    (define lookup (make-lookup index))
+    (define lookup (make-lookup fields))
     (λ (fv) (tuple lookup fv)))
 
-(define (relation fields (tuples empty))
-    (define index (make-index fields))
-    (define lookup (make-lookup index))
+(define (relation name fields (tuples empty))
+    (define lookup (make-lookup fields))
     (define (convert lss) (map list->fector lss))
     (define (get t f)
 	(fector-ref t (lookup f)))
@@ -103,6 +116,10 @@
 			(map (λ (f) (get v f)) wanted))))
 	    (map grab (filter is-match? ts)))
 	(define (read-row where xs) (car (read where xs)))
+	(define (read-col where xs)
+	    (define colname (car xs))
+	    (for/list ((tup (in-list (read where xs))))
+		(tup colname)))
 	(define (read-val where xs) ((read-row where xs) (car xs)))
 	(define (update where put)
 	    (define is-match? (mk-matcher where))
@@ -124,86 +141,26 @@
 	    (define is-match? (mk-matcher where))
 	    (mk-relation (filter-not is-match? ts)))
 	(define (serialize)
-	    (hash 'fields fields 'tuples (map fector->vector ts)))
-	(define dispatch
-	    (case-lambda
-		(() (serialize))
-		((cmd args)
-		    (case cmd
-			('create (create args))
-			('delete (delete args))))
-		((cmd where xs)
-		    (case cmd
-			('read (read where xs))
-			('read-row (read-row where xs))
-			('read-val (read-val where xs))
-			('update (update where xs))))))
-	dispatch)
+	    (hash
+		'name name
+		'fields fields
+		'tuples (map fector->vector ts)))
+	(case-lambda
+	    (() (serialize))
+	    ((cmd)
+		(case cmd
+		    ('name name)
+		    ('fields fields)))
+	    ((cmd args)
+		(case cmd
+		    ('create (create args))
+		    ('delete (delete args))))
+	    ((cmd where xs)
+		(case cmd
+		    ('read (read where xs))
+		    ('read-row (read-row where xs))
+		    ('read-col (read-col where xs))
+		    ('read-val (read-val where xs))
+		    ('update (update where xs))))))
     (mk-relation (convert tuples)))
-
-(define fields '(x y z p q))
-(define tf (tuple-factory fields))
-(define xyzpq (tf '(1 1 2 3 5)))
-
-(newline)
-
-(xyzpq)
-(xyzpq 'z)
-((xyzpq 'q 8))
-
-(newline)
-
-(define ts
-    '((1 1 2 3 5)
-      (8 13 21 34 55)
-      (0 0 2 1 0)  
-      (1 1 1 1 1)
-      (0 0 0 0 0)))
-
-(define table (relation fields ts))
-
-(define (unroll xs)
-    (map (λ (tup) (tup)) xs))
-
-
-(unroll (table 'read '((x 0) (q 0)) '(y z p)))
-((table 'read-row '((p 3)) '(x y z)))
-(table 'read-val '((z 21)) '(q))
-
-(unroll
-    (table 'read
-	(λ (tup)
-	    (let ((total (apply + (map tup fields))))
-		(or (= (tup 'z) 21) (and (> total 4) (< total 16)))))
-	'(x y z p q)))
-
-(newline)
-
-(table)
-
-(define table2 (table 'create '((8 6 7 5 3) (0 9 8 6 7) (5 3 0 9 9))))
-
-(table2)
-
-(define table3 (table2 'update '((x 0)) '((y 3))))
-
-(table3)
-
-(define table4 (table3 'update
-    (λ (t) (> (apply + (map t fields)) 10))
-    (λ (t) (let ((xyz (map t '(x y z))))
-	 (append xyz (list (apply + xyz) (apply * xyz)))))))
-
-(table4)
-
-(define table5 (table4 'delete (λ (t) (> (apply + (map t fields)) 100))))
-
-(table5)
-
-(define table6 (table5 'delete '((x 1))))
-
-(table6)
-
-
-
 
--- tests.rkt
+++ tests.rkt
@@ -54,5 +54,73 @@
 (define new-world (create-datastore (ds init)))
 
 
+(define fields '(x y z p q))
+(define tf (tuple-factory fields))
+(define xyzpq (tf '(1 1 2 3 5)))
+
+(newline)
+
+(xyzpq)
+(xyzpq 'z)
+((xyzpq 'q 8))
+
+(newline)
+
+(define ts
+    '((1 1 2 3 5)
+      (8 13 21 34 55)
+      (0 0 2 1 0)  
+      (1 1 1 1 1)
+      (0 0 0 0 0)))
+
+(define table (relation 'numbers fields ts))
+
+(define (unroll xs)
+    (map (λ (tup) (tup)) xs))
+
+(table 'name)
+(table 'fields)
+
+(unroll (table 'read '((x 0) (q 0)) '(y z p)))
+((table 'read-row '((p 3)) '(x y z)))
+(table 'read-val '((z 21)) '(q))
+
+(unroll
+    (table 'read
+	(λ (tup)
+	    (let ((total (apply + (map tup (table 'fields)))))
+		(or (= (tup 'z) 21) (and (> total 4) (< total 16)))))
+	'(x y z p q)))
+
+(newline)
+
+(table)
+
+(define table2 (table 'create '((8 6 7 5 3) (0 9 8 6 7) (5 3 0 9 9))))
+
+(table2)
+
+(define table3 (table2 'update '((x 0)) '((y 3))))
+
+(table3)
+
+(define table4 (table3 'update
+    (λ (t) (> (apply + (map t (table3 'fields))) 10))
+    (λ (t) (let ((xyz (map t '(x y z))))
+	 (append xyz (list (apply + xyz) (apply * xyz)))))))
+
+(table4)
+
+(define table5 (table4 'delete (λ (t) (> (apply + (map t (table4 'fields))) 100))))
+
+(table5)
+
+(define table6 (table5 'delete '((x 1))))
+
+(table6)
+
+
+
+
 
 
